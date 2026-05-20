715d107a1f61c7346ae5f17b998b8d784c34dbd3
alvaro rodriguez <albarogarcia2002@gmail.com>
2026-05-20 01:54:30 -0500

    Corrigiendo rutas entre paginas

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/conocenos')
+def conocenos():
+    return render_template('conocenos.html')
+
+@app.route('/redes')
+def redes():
+    return render_template('redes.html')
+
 if __name__ == "__main__":
     app.run()
